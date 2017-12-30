0952512506ee43f69fb767d18bb567f5bb82eac5
krishnaKapadia <kapadikris@myvuw.ac.nz>
2017-12-30 13:17:36 +1300

    setup http routes

---
+++ Backend-API/Routes/routes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router  = express.Router();
+
+// GET request handler, req = request, res = responce
+router.get("/clients", function(req, res) {
+    console.log("Getting");
+    res.send({ clientName: "Krishna" });
+});
+
+// POST request handler
+router.post("/clients", function(req, res) {
+    res.send({ clientName: "Krishna" });
+});
+
+// PUT request handler, Updates client matching id
+router.put("/clients/:id", function(req, res) {
+    console.log("Getting");
+    res.send({ clientName: "Krishna" });
+});
+
+// DELETE request handler
+router.delete("/clients/:id", function(req, res) {
+    res.send({ clientName: "Krishna" });
+});
+
+module.exports = router;
--- Backend-API/index.js
+++ Backend-API/index.js
@@ -1,15 +1,12 @@
 // Base imports
 const express = require('express');
 const app = express();
+const routes = require('./routes/routes');
+
+// Custom route Middleware, ....com/api/...
+app.use("/api", routes);
 
 // Port Listining on server port var or port 4000
 app.listen(process.env.port || 4000, function() {
     console.log("check");
-});
-
-
-// GET request handler, req = request, res = responce
-app.get("/", function(req, res) {
-    console.log("Getting");
-    res.send({ clientName: "Krishna" });
 });
