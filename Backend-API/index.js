9cb7712cc7ed3f50d9f7c3b9f5a32f9798627050
KrishnaKapadia <kapadikris@myvuw.ac.nz>
2018-01-01 12:58:10 +1300

    Setup mysql database connection

--- Backend-API/index.js
+++ Backend-API/index.js
@@ -2,9 +2,26 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes/routes');
+var mysql = require('mysql');
+
+//Db connection
+app.use(function(req, res, next) {
+  res.locals.connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'test'
+  });
+
+  res.locals.connection.connect();
+  next();
+});
+
+// Middleware
 
 // Custom route Middleware, ....com/api/...
-app.use("/api", routes);
+app.use("/api/v1", routes);
+
 
 // Port Listining on server port var or port 4000
 app.listen(process.env.port || 4000, function() {
