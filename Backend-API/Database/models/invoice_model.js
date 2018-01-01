2ce1bef52e92452f2226282d432074b882a678a4
KrishnaKapadia <kapadikris@myvuw.ac.nz>
2018-01-01 23:25:39 +1300

    Started invoice model, not completed yet

---
+++ Backend-API/Database/models/invoice_model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+/**
+ * Model/Schema for Invoice Document in Mongo
+ */
+
+// Base Schema
+const invoiceSchema = new Schema({
+  code: {
+    type: number,
+    required: [true, "Every Invoice must have an invoice number"]
+  },
+
+
+
+});
+
+const Invoice = mongoose.model('invoice', invoiceSchema);
+
+module.exports = Invoice;
