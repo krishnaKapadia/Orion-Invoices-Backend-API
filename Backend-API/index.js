79358066e0d112173b6436d7beb2953cd05e8b8b
KrishnaKapadia <kapadikris@myvuw.ac.nz>
2018-01-01 13:02:47 +1300

    Setup mysql connection.

--- Backend-API/index.js
+++ Backend-API/index.js
@@ -1,10 +1,11 @@
 // Base imports
-<<<<<<< HEAD
 const express = require('express');
 const app = express();
 const routes = require('./routes/routes');
 var mysql = require('mysql');
 
+// Constants
+const PORT = 4000;
 //Db connection
 app.use(function(req, res, next) {
   res.locals.connection = mysql.createConnection({
@@ -23,22 +24,8 @@ app.use(function(req, res, next) {
 // Custom route Middleware, ....com/api/...
 app.use("/api/v1", routes);
 
-=======
-const express    = require('express');
-const app        = express();
-const routes     = require('./routes/routes');
-const bodyParser = require('body-parser');
-
-const port = 4000;
-
-// Middleware, remember that the decleration order matters
-app.use(bodyParser.json());
-
-// Custom routes, ....com/api/...
-app.use("/api", routes);
->>>>>>> 24c6e05e25dc64f24952773e65bc7f3c9d71a87b
 
 // Port Listening on server port var or port 4000
-app.listen(process.env.port || port, function() {
+app.listen(process.env.port || PORT, function() {
     console.log("check");
 });
